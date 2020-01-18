76abb78c33f1ecc02fd4479e31ee43a87912f9bf
Ananth Narayan S <ananth.narayan@gmail.com>
2020-01-18 14:03:55 +0530

    Fix parser issues caused by unit field.
    Some metrics had unit field which caused the less columns being cut.

--- scripts/parser-script.sh
+++ scripts/parser-script.sh
@@ -3,15 +3,22 @@
 ##
 declare -a events;
 events=( "cpu-clock" "context-switches" "cpu-migrations" "page-faults" "cycles" "instructions" "branches" "branch-misses" 
-	 "L1-dcache-loads" "L1-dcache-load-misses" "LLC-loads" "LLC-loads-misses" "L1-icache-loads" "L1-icache-load-misses"
-	 "dTLB-loads" "dTLB-loads-misses" "iTLB-loads" "iTLB-load-misses" "L1-dcache-prefetches" "L1-dcache-prefetch-misses") #List of events that we want to extract from the files
-#This below set of events is where some the events that were marked as 'not supported' are removed from the above list.	 
+	 "L1-dcache-loads" "L1-dcache-load-misses" "LLC-loads" "LLC-load-misses" "L1-icache-loads" "L1-icache-load-misses"
+	 "dTLB-loads" "dTLB-load-misses" "iTLB-loads" "iTLB-load-misses" "L1-dcache-prefetches" "L1-dcache-prefetch-misses") #List of events that we want to extract from the files
+# This below set of events is where some the events that were marked as 'not supported' are removed from the above list.	 
 #events=( "cpu-clock" "context-switches" "cpu-migrations" "page-faults" "cycles" "instructions" "branches" "branch-misses" 
 #	 "L1-dcache-loads" "L1-dcache-load-misses" "LLC-loads" "LLC-loads-misses"  "L1-icache-load-misses"
 #	 "dTLB-loads" "dTLB-loads-misses" "iTLB-loads" "iTLB-load-misses" ) #List of events that we want to extract from the files
 	 
-#list of input files, one for each benchmark that we run
-perf_output_files=("events.perf") 
+# List of input files, one for each benchmark that we run
+# Assume that all files are present in the same directory
+#perf_output_files=("events.perf") 
+if [ $# -eq 0 ];
+then
+	echo "Missing command line arguments"
+	exit
+fi
+perf_output_files=("$@")
 #list of output files, one for each benchmark that we run
 final_output_files=("final_events.csv")
 #Create the final output file. If one exists previously, delete it
@@ -19,24 +26,28 @@ final_output_files=("final_events.csv")
 tempfile="event_temp.perf"
 intermediate_csv="intermediate.csv"
 final_out="final_out.csv" 
-rm $tempfile $intermediate_csv $final_out 
+#rm $tempfile $intermediate_csv $final_out 
 touch $final_out
+touch $intermediate_csv
 
-#for file in "${perf_output_files[@]}"
 count=${#perf_output_files[@]}
 echo "Processing a total of $count files"
 for ((i=0;i<$count;i++))
 do
 	file=${perf_output_files[$i]}
-	cat $file | tr -s " " | sed "s/<not supported>/-1/g" | cut -d " " -f 1-4 > $tempfile 
+	#there is a 'unit' header for some metrics such as cpu-clock.
+	#so we need 5 columns. This adds the # rows where unit is not
+	#applicable. Better way would be to use sed and replace the unit
+	#with a tab, but for that we need a list of all units
+	cat $file | tr -s " " | sed "s/<not supported>/-1/g" | cut -d " " -f 1-5 > $tempfile 
 ## Extract each event from the perf output and
 ## save in separate files
 	for event in "${events[@]}"
 	do
 		echo "Extracting $event from $tempfile"
-		#header
+		# Print a header
 		echo -e "$event" > $event.txt
-		grep $event $tempfile  | tr -s " " | sed "s/<not supported/-1/g" |  cut -d " " -f 3 >> $event.txt
+		grep $event $tempfile  |  cut -d " " -f 3 >> $event.txt
 		mv $final_out $intermediate_csv
 		paste -d ";" $intermediate_csv $event.txt > $final_out
 	done
