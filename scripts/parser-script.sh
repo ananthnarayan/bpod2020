2da36c9d48299e8852546934691cae3248adfe20
Ananth Narayan S <ananth.narayan@gmail.com>
2020-01-30 18:39:31 +0530

    Edited grep command to prevent extraneous matches
    Certain event names are part of other ones - cycles for ex, is
    part of another event. So additional lines were getting matched
    by grep.

--- scripts/parser-script.sh
+++ scripts/parser-script.sh
@@ -2,9 +2,13 @@
 ## Author : Ananth Narayan S
 ##
 declare -a events;
-events=( "cpu-clock" "context-switches" "cpu-migrations" "page-faults" "cycles" "instructions" "branches" "branch-misses" 
+#events=( "cpu-clock" "context-switches" "cpu-migrations" "page-faults" "cycles" "instructions" "branches" "branch-misses" 
+#	 "L1-dcache-loads" "L1-dcache-load-misses" "LLC-loads" "LLC-load-misses" "L1-icache-loads" "L1-icache-load-misses"
+#	 "dTLB-loads" "dTLB-load-misses" "iTLB-loads" "iTLB-load-misses" "L1-dcache-prefetches" "L1-dcache-prefetch-misses") #List of events that we want to extract from the files
+events=( "page-faults" "cycles" "instructions" "branches" "branch-misses" 
 	 "L1-dcache-loads" "L1-dcache-load-misses" "LLC-loads" "LLC-load-misses" "L1-icache-loads" "L1-icache-load-misses"
 	 "dTLB-loads" "dTLB-load-misses" "iTLB-loads" "iTLB-load-misses" "L1-dcache-prefetches" "L1-dcache-prefetch-misses") #List of events that we want to extract from the files
+
 # This below set of events is where some the events that were marked as 'not supported' are removed from the above list.	 
 #events=( "cpu-clock" "context-switches" "cpu-migrations" "page-faults" "cycles" "instructions" "branches" "branch-misses" 
 #	 "L1-dcache-loads" "L1-dcache-load-misses" "LLC-loads" "LLC-loads-misses"  "L1-icache-load-misses"
@@ -20,7 +24,6 @@ then
 fi
 perf_output_files=("$@")
 #list of output files, one for each benchmark that we run
-final_output_files=("final_events.csv")
 #Create the final output file. If one exists previously, delete it
 
 tempfile="event_temp.perf"
@@ -35,11 +38,14 @@ echo "Processing a total of $count files"
 for ((i=0;i<$count;i++))
 do
 	file=${perf_output_files[$i]}
+	final_output_file=`basename $file .data`
+	final_output_file=${final_output_file}.csv
+	echo "Writing to $final_output_file"
 	#there is a 'unit' header for some metrics such as cpu-clock.
 	#so we need 5 columns. This adds the # rows where unit is not
 	#applicable. Better way would be to use sed and replace the unit
 	#with a tab, but for that we need a list of all units
-	cat $file | tr -s " " | sed "s/<not supported>/-1/g" | cut -d " " -f 1-5 > $tempfile 
+	cat $file | tr -s " " | sed "s/<not supported>/-1/g" | cut -d ";" -f 1-5 > $tempfile 
 ## Extract each event from the perf output and
 ## save in separate files
 	for event in "${events[@]}"
@@ -47,11 +53,11 @@ do
 		echo "Extracting $event from $tempfile"
 		# Print a header
 		echo -e "$event" > $event.txt
-		grep $event $tempfile  |  cut -d " " -f 3 >> $event.txt
+		grep ";$event" $tempfile  |  cut -d ";" -f 2 >> $event.txt
 		mv $final_out $intermediate_csv
 		paste -d ";" $intermediate_csv $event.txt > $final_out
 	done
-	mv final_out.csv ${final_output_files[$i]}
+	mv final_out.csv ${final_output_file}
 
 ## Merge the separate files into one
 ## Merge has to be column-wise, with comma as separator
