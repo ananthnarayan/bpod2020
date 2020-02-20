09167bd87054a08a232bbdd2d359abb109f24c43
Ananth Narayan S <ananth.narayan@gmail.com>
2020-02-20 15:46:57 +0530

    Remove Dead Code

--- scripts/parser-script.sh
+++ scripts/parser-script.sh
@@ -2,6 +2,8 @@
 ## Author : Ananth Narayan S
 ##
 declare -a events;
+declare -a eventNames;
+declare -a eventNamesString;
 #events=( "cpu-clock" "context-switches" "cpu-migrations" "page-faults" "cycles" "instructions" "branches" "branch-misses" 
 #	 "L1-dcache-loads" "L1-dcache-load-misses" "LLC-loads" "LLC-load-misses" "L1-icache-loads" "L1-icache-load-misses"
 #	 "dTLB-loads" "dTLB-load-misses" "iTLB-loads" "iTLB-load-misses" "L1-dcache-prefetches" "L1-dcache-prefetch-misses") #List of events that we want to extract from the files
@@ -17,11 +19,11 @@ declare -a events;
 #01C2 : uops retired- all (Broadwell)
 #0108 : DTLB misses
 #0185 : itlb miss
+events=('00C0' '02C0' '4f2e' '412e' '003c' '01c2' '0108' '0185' 'intel_cqm/llc_occupancy/' 'intel_cqm/total_bytes/' 'intel_cqm/local_bytes/' )
+eventNames=('Instructions' 'FPInstructions' 'LLCRef' 'LLCMiss' 'Cycles' 'Uops' 'DTLBMiss' 'ITLBMiss' 'LLCOcc' 'TotalBytes' 'LocalBytes')
+#Turns out we didn't use this string at all. :) 
+eventNamesString='Instructions,FPInstructions,LLCRef,LLCMiss,Cycles,Uops,DTLBMiss,ITLBMiss,LLCOcc,TotalBytes,LocalBytes'
 
-events=('r00C0' 'r02C0' 'r4f2e' 'r412e' 'r003c' 'r01c2' 'r0108' 'r0185' )
-eventNames=('Instructions' 'FPInstructions' 'LLCRef' 'LLCMiss' 'Cycles' 'Uops' 'DTLBMiss' 'ITLBMiss')
-eventNamesString='Instructions,FPInstructions,LLCRef,LLCMiss,Cycles,Uops,DTLBMiss,ITLBMiss'
-	 
 # List of input files, one for each benchmark that we run
 # Assume that all files are present in the same directory
 if [ $# -eq 0 ];
@@ -36,7 +38,6 @@ perf_output_files=("$@")
 tempfile="event_temp.perf"
 intermediate_csv="intermediate.csv"
 final_out="final_out.csv" 
-#rm $tempfile $intermediate_csv $final_out 
 
 eventsCount=${#events[@]}
 rm *.txt
@@ -45,7 +46,6 @@ count=${#perf_output_files[@]}
 echo "Processing a total of $count files"
 for ((i=0;i<$count;i++))
 do
-	echo $i
 	touch $final_out
 	touch $intermediate_csv
 	file=${perf_output_files[$i]}
@@ -56,19 +56,17 @@ do
 	#If any event is not supported then mark it with a -1. This has to 
 	#be ignored or handled as null/invalid data in subsequent processing.
 	cat $file | tr -s " " | sed "s/<not supported>/-1/g" | cut -d ";" -f 1-5 > $tempfile 
-## Extract each event from the perf output and save it into separate files. 
-	#for event in "${events[@]}"
+	## Extract each event from the perf output and save it into separate files. 
 	for((j=0;j<$eventsCount;j++))
 	do
 		event=${events[$j]}
-		echo "Extracting $event from $tempfile"
+		echo -e "\tExtracting $event from $tempfile"
 		# Print a header
 		echo -e "${eventNames[$j]}" > $event.txt
 		grep -i "$event" $tempfile  |  cut -d "," -f 2 >> $event.txt
 		mv $final_out $intermediate_csv
 		paste -d "," $intermediate_csv $event.txt > $final_out
 	done
-	#echo "final_out.csv -> ${final_output_file}"
 	mv final_out.csv ${final_output_file}
 	sync
 	sleep 1
@@ -77,16 +75,12 @@ do
 ## Merge the separate files into one
 ## Merge has to be column-wise, with comma as separator
 ## giving us a nice csv
-	echo $i
 done
 echo "Done"
-#rm $intermediate_csv
-#rm $tempfile
 
 ###
 # It is best that we run perf with the -x. Else, perf
 # does number formatting with commas which might cause trouble
 # parsing. 
-# perf stat -d -d -d -o events.txt  -I 1000 -x "\t"
-# grep "cpu-clock" events.txt | tr -s "\t" | cut -d$'\t' -f 2
+# perf stat  -e blah -o events.txt  -I 1000 -x ","
 ###
