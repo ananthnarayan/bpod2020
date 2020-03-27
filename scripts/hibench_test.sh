c8867e4501023566b60845e8a3ec3df0155742f8
Ananth Narayan S <ananth.narayan@gmail.com>
2020-03-27 12:26:29 +0530

    Edited hibench exec script

--- scripts/hibench_test.sh
+++ scripts/hibench_test.sh
@@ -2,7 +2,7 @@
 
 set_remote_hadoop_home()
 {
-    HADOOP_HOME=/disk2/user/hadoops/
+    HADOOP_HOME=/disk2/user/hadoops/hadoop-3.2.1
 }
 cleanup (){
 	ssh user@192.168.122.23 -C $HADOOP_HOME/bin/hdfs dfs -rm -r /HiBench/$1 
@@ -11,7 +11,9 @@ expunge () {
     ssh user@192.168.122.23 -C $HADOOP_HOME/bin/hdfs dfs -expunge 
 }
 
-
+cleanup_bdb() {
+	ssh user@192.168.122.23 -C $HADOOP_HOME/bin/hdfs fds -rm -r /hadoop/$1
+}
 run_remote_workload() {
     vmpid=$1
     bench=$2
@@ -21,16 +23,15 @@ run_remote_workload() {
     time_log=$5
     
     echo "===== $bench : $action ======"
+    killall perf
     perf stat -e intel_cqm/llc_occupancy/ -e intel_cqm/local_bytes/ -e intel_cqm/total_bytes/ -e r00C0 -e r00c4  -e r00c5  -e r4f2e -e r412e -e r003C -e r0108 -e r0185 -p $vmpid -x "," -o $file -I 500 &
     perfprocess=$!
-    
+    sleep 1 
     command="/usr/bin/time -f %e,%S,%U,%W,%c,%w -o timeout ssh user@192.168.122.23 -i id_rsa -C bash /disk2/user/HiBench/bin/workloads/$remote_command"
     
     eval $command
     time=`cat timeout` 
     kill -s SIGINT $perfprocess
-    bench='sort'
-    action="prep"
     echo -e "$bench,$action,$time" >> $time_log
     
     echo "Start sleep after $action"
@@ -38,53 +39,85 @@ run_remote_workload() {
 
 }
 
+run_remote_workload_bdb() {
+    vmpid=$1
+    bench=$2
+    action=$3
+    file=${bench}_${action}.perf
+    remote_command=$4
+    time_log=$5
+    
+    echo "===== $bench : $action ======"
+    killall perf
+    perf stat -e intel_cqm/llc_occupancy/ -e intel_cqm/local_bytes/ -e intel_cqm/total_bytes/ -e r00C0 -e r00c4  -e r00c5  -e r4f2e -e r412e -e r003C -e r0108 -e r0185 -p $vmpid -x "," -o $file -I 500 &
+    perfprocess=$!
+    sleep 1 
+    command="/usr/bin/time -f %e,%S,%U,%W,%c,%w -o timeout ssh user@192.168.122.23 -i id_rsa -C bash /disk2/user/BigDataBench_V5.0_BigData_MicroBenchmark/Hadoop/$remote_command"
+    
+    eval $command
+    time=`cat timeout` 
+    kill -s SIGINT $perfprocess
+    echo -e "$bench,$action,$time" >> $time_log
+    
+    echo "Start sleep after $action"
+    sleep 10
+
+}
 vmpid=`ps -elf | grep BUS_2 | grep qemu | tr -s " " | cut -d " " -f 4`
-time_log='hibench_time.log'
+time_log='benchmark_time.log'
 rm $time_log
 
 echo "================="
-profile="tiny"
-echo "VM PID: $vmpid Profile:$profile" 
+profile="large"
+hadoop="hadoop-3.2.1"
 
+echo "VM PID: $vmpid Profile:$profile" 
+killall perf
 set_remote_hadoop_home 
 echo "Current Hadoop Home is: $HADOOP_HOME"
 
 run_remote_workload $vmpid "sort" "prep" "micro/sort/prepare/prepare.sh" $time_log
-run_remote_workload $vmpid "sort" "run"     "micro/sort/hadoop/run.sh"      $time_log
+run_remote_workload $vmpid "sort" "run"  "micro/sort/hadoop/run.sh"      $time_log
 cleanup "Sort"
 
-run_remote_workload $vmpid "sort" "prep" "micro/terasort/prepare/prepare.sh" $time_log
-run_remote_workload $vmpid "sort" "run"  "micro/terasort/hadoop/run.sh"      $time_log
+run_remote_workload $vmpid "terasort" "prep" "micro/terasort/prepare/prepare.sh" $time_log
+run_remote_workload $vmpid "terasort" "run"  "micro/terasort/hadoop/run.sh"      $time_log
 cleanup "Terasort"
 
-run_remote_workload $vmpid "sort" "prep" "micro/wordcount/prepare/prepare.sh" $time_log
-run_remote_workload $vmpid "sort" "run"  "micro/wordcount/hadoop/run.sh"      $time_log
+run_remote_workload $vmpid "wordcount" "prep" "micro/wordcount/prepare/prepare.sh" $time_log
+run_remote_workload $vmpid "wordcount" "run"  "micro/wordcount/hadoop/run.sh"      $time_log
 cleanup "Wordcount"
 
-run_remote_workload $vmpid "sort" "prep" "ml/bayes/prepare/prepare.sh" $time_log
-run_remote_workload $vmpid "sort" "run"  "ml/bayes/hadoop/run.sh"      $time_log
-cleanup "Bayes"
+#run_remote_workload $vmpid "bayes" "prep" "ml/bayes/prepare/prepare.sh" $time_log
+#run_remote_workload $vmpid "bayes" "run"  "ml/bayes/hadoop/run.sh"      $time_log
+#cleanup "Bayes"
 
-run_remote_workload $vmpid "sort" "prep" "ml/kmeans/prepare/prepare.sh" $time_log
-run_remote_workload $vmpid "sort" "run"  "ml/kmeans/hadoop/run.sh"      $time_log
-cleanup "Kmeans"
+#run_remote_workload $vmpid "kmeans" "prep" "ml/kmeans/prepare/prepare.sh" $time_log
+#run_remote_workload $vmpid "kmeans" "run"  "ml/kmeans/hadoop/run.sh"      $time_log
+#cleanup "Kmeans"
 
-run_remote_workload $vmpid "sort" "prep" "sql/aggregation/prepare/prepare.sh" $time_log
-run_remote_workload $vmpid "sort" "run"  "sql/aggretation/hadoop/run.sh"      $time_log
+run_remote_workload $vmpid "aggregation" "prep" "sql/aggregation/prepare/prepare.sh" $time_log
+run_remote_workload $vmpid "aggregation" "run"  "sql/aggregation/hadoop/run.sh"      $time_log
 cleanup "Aggregation"
-
-run_remote_workload $vmpid "sort" "prep" "sql/join/prepare/prepare.sh" $time_log
-run_remote_workload $vmpid "sort" "run"  "sql/join/hadoop/run.sh"      $time_log
+#
+run_remote_workload $vmpid "join" "prep" "sql/join/prepare/prepare.sh" $time_log
+run_remote_workload $vmpid "join" "run"  "sql/join/hadoop/run.sh"      $time_log
 cleanup "Join"
 
-run_remote_workload $vmpid "sort" "prep" "sql/scan/prepare/prepare.sh" $time_log
-run_remote_workload $vmpid "sort" "run"  "sql/scan/hadoop/run.sh"      $time_log
-cleanup "Scan"
+run_remote_workload $vmpid "scan" "prep" "sql/scan/prepare/prepare.sh" $time_log
+run_remote_workload $vmpid "scan" "run"  "sql/scan/hadoop/run.sh"      $time_log
+#cleanup "Scan"
 
-run_remote_workload $vmpid "sort" "prep" "websearch/nutchindexing/prepare/prepare.sh" $time_log
-run_remote_workload $vmpid "sort" "run"  "websearch/nutchindexing/hadoop/run.sh"      $time_log
-cleanup "Nutchindexing"
+run_remote_workload $vmpid "nutchindexing" "prep" "websearch/nutchindexing/prepare/prepare.sh" $time_log
+run_remote_workload $vmpid "nutchindexing" "run"  "websearch/nutchindexing/hadoop/run.sh"      $time_log
+#cleanup "Nutchindexing"
 
-run_remote_workload $vmpid "sort" "prep" "websearch/pagerank/prepare/prepare.sh" $time_log
-run_remote_workload $vmpid "sort" "run"   "websearch/pagerank/run.sh"      $time_log
-cleanup "Pagerank"
+#run_remote_workload $vmpid "pagerank" "prep" "websearch/pagerank/prepare/prepare.sh" $time_log
+#run_remote_workload $vmpid "pagerank" "run"   "websearch/pagerank/hadoop/run.sh"      $time_log
+#cleanup "Pagerank"
+
+
+expunge
+mkdir -p $hadoop/$profile
+mv *.perf $hadoop/$profile
+mv $time_log $hadoop/$profile
