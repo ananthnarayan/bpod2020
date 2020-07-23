a280637da1eb5cfffcec694e6192335c0930c753
Ananth Narayan S <ananth.narayan@gmail.com>
2020-07-23 11:29:27 +0530

    Use jps to get pids of hadoop processes. other minor updates.

--- scripts/bdb_hadoop_pidstat.sh
+++ scripts/bdb_hadoop_pidstat.sh
@@ -1,6 +1,7 @@
 set_remote_hadoop_home()
 {
     HADOOP_HOME=/disk2/user/hadoops/hadoop-$1
+    echo "HADOOP_BASE=$HADOOP_HOME" > ~/BigDataBench_V5.0_BigData_MicroBenchmark/Hadoop/hadoophome.sh
 }
 
 cleanup_bdb () 
@@ -10,6 +11,7 @@ cleanup_bdb ()
 cleanup_bdb_cc ()
 {
     $HADOOP_HOME/bin/hdfs dfs -rm -r /user/user/*
+    $HADOOP_HOME/bin/hdfs dfs -rm -r /user/ananth/*
 }
 check_bdb_conf()
 {
@@ -42,72 +44,75 @@ run_remote_workload_bdb() {
     time_log=$5
     
     #Clean up any old instances
-    killall perf 
     killall sar 
+    killall pidstat
     
     echo "===== $bench : $action ======"
+    echo "$6" 
     case $6 in
     "set1")
     	   perfcommand='perf stat -e intel_cqm/llc_occupancy/ -e intel_cqm/local_bytes/ -e intel_cqm/total_bytes/ -e r00C0 -e r00c4  -e r00c5  -e r4f2e -e r412e -e r003C  -x "," -o $file -I $delay'
-           sar -r ALL -B -W -o $sar_file $sar_delay > /dev/null 2>&1 & 
+           sar -r ALL -R -B -W -S -u ALL -H -b -o $sar_file $sar_delay > /dev/null 2>&1 & 
            sar_process=$!
 	       ;;
     "set2")
             perfcommand='perf stat -e r3f24 -e ref24 -e r0151 -e r0480 -e context-switches -e page-faults  -x "," -o $file -I $delay'
-            sar -b -u ALL -o $sar_file $sar_delay > /dev/null 2>&1 & 
+           sar -r ALL -R -B -W -S -u ALL -H -h -o $sar_file $sar_delay > /dev/null 2>&1 & 
             sar_process=$!
             ;;
     "set3")
-          #perf stat -e cpu/event=0xa3,umask=10,cmask=16,name=Cycle_Activity.Cycles_Mem_Any/ -e cpu/event=0xa3,umask=14,cmask=20,name=Cycle_Activity.Stalls_MemAny/  -e r81d0 -e r82d0  -p $vmpid -x "," -o $file -I $delay &
             perfcommand='perf stat  -e r81d0 -e r82d0  -x "," -o $file -I $delay '
-            sar -r ALL -B -W -o $sar_file $sar_delay > /dev/null 2>&1 & 
+           sar -r ALL -R -B -W -S -u ALL -H -h -o $sar_file $sar_delay > /dev/null 2>&1 & 
             sar_process=$!
             ;;
     esac
     sync #flush all buffers. 
     sleep 1
-    command="/usr/bin/time -f %e,%S,%U,%W,%c,%w -o timeout $perfcommand bash /disk2/user/BigDataBench_V5.0_BigData_MicroBenchmark/Hadoop/$remote_command  &"
+    #command="/usr/bin/time -f %e,%S,%U,%W,%c,%w -o timeout $perfcommand bash /disk2/user/BigDataBench_V5.0_BigData_MicroBenchmark/Hadoop/$remote_command  &"
+    command="bash /disk2/user/BigDataBench_V5.0_BigData_MicroBenchmark/Hadoop/$remote_command  &"
     echo "Executing: $command"
     eval $command
+#26546 SecondaryNameNode
+#26116 NameNode
+#26279 DataNode
+#27052 NodeManager
+#27230 Jps
+#26847 ResourceManager
+
     timepid=$!
-    perfpid=`ps -elf | grep "$remote_command" | grep -v "/usr/bin/time" | grep -v "grep" | tr -s " " | cut -d " " -f 4`
-    bashpid=`ps -elf | grep "$remote_command" | grep -v "/usr/bin/time" | grep -v "perf" | grep -v "grep" |   tr -s " " | cut -d " " -f 4`
+    bashpid=`ps -elf | grep "$remote_command" | grep -v "/usr/bin/time" |  grep -v "grep" |   tr -s " " | cut -d " " -f 4`
     sleep 3
     javapid=`ps -elf | grep "$bashpid" | grep -v "grep" | tail -n 1 | tr -s " " | cut -d ' ' -f 4`
-
-    namenode=`ps -elf | grep namenode | grep -v "grep" | tail -n 1 | tr -s " " | cut -d ' ' -f 4`
-    datanode=`ps -elf | grep datanode | grep -v "grep" | tail -n 1 | tr -s " " | cut -d ' ' -f 4`
-    nodemanager=`ps -elf | grep nodemanager | grep -v "grep" | tail -n 1 | tr -s " " | cut -d ' ' -f 4`
-    resourcemanager=`ps -elf | grep resourcemanager | grep -v "grep" | tail -n 1 | tr -s " " | cut -d ' ' -f 4`
-    secondarynamenode=`ps -elf | grep secondarynamenode | grep -v "grep" | tail -n 1 | tr -s " " | cut -d ' ' -f 4`
-
+    namenode=`jps | grep -i -w  "NameNode" | cut -d " " -f 1`
+    datanode=`jps | grep -i -w  "DataNode" | cut -d " " -f 1`
+    nodemanager=`jps | grep -i -w  "NodeManager" | cut -d " " -f 1`
+    resourcemanager=`jps | grep -i -w  "ResourceManager" | cut -d " " -f 1`
+    secondarynamenode=`jps | grep -i -w  "SecondaryNameNode" | cut -d " " -f 1`
      
-    echo -e "Perfpid: $perfpid Bash: $bashpid Java: $javapid Hadoop: $namenode,$datanode,$nodemanager,$resourcemanager,$secondarynamenode"
-    pidstat -h -r -s -T ALL -p $namenode,$datanode,$nodemanager,$resourcemanager,$secondarynamenode $sar_delay > $pidstat_file &  
-    time=`cat timeout` 
-    wait $timepid
-    echo "killing sar"
+    echo -e "Sar:$sar_process Bash: $bashpid Java: $javapid Hadoop: $namenode,$datanode,$nodemanager,$resourcemanager,$secondarynamenode"
+    pidstat -h -d -r -s -u -T ALL -p $javapid,$namenode,$datanode,$nodemanager,$resourcemanager,$secondarynamenode $sar_delay > $pidstat_file  & 
+    pidstat=$!
+    wait $bashpid
+    echo "Terminating sar"
     kill -s SIGINT $sar_process
     #Pidstat would have terminated already. this is just for cleanup from our side. 
-    echo "killing pidstat"
+    echo "Terminating pidstat"
     kill -s SIGINT $pidstat 
-
+#sar -r ALL -R -B -W -S -u ALL -H -b -o some_file 1
     case $6 in
     	"set1")
-	    sadf -dh $sar_file -- -r ALL -B -W > $sar_csv
+            sadf -dh $sar_file -- -r ALL -R -B -W -S -u ALL -H -b > $sar_csv 
 	    ;;
 	"set2")
-		sadf -dh $sar_file -- -b -u  > $sar_csv
-		;;
+            sadf -dh $sar_file -- -r ALL -R -B -W -S -u ALL -H -b > $sar_csv 
+	    ;;
 	"set3")
-		sadf -dh $sar_file -- -r ALL -B -W > $sar_csv
-		;;
+            sadf -dh $sar_file -- -r ALL -R -B -W -S -u ALL -H -b > $sar_csv 
+	    ;;
     esac 
     
     bench=$bench
     action=$action
-    echo -e "$bench,$action,$time" >> $time_log
-    
     echo "Start sleep after $action"
     sleep 10
 
@@ -115,13 +120,15 @@ run_remote_workload_bdb() {
 
 ### == end function definitions == ###
 check_bdb_conf
-time_log='benchmarks_time.log'
-sudo rm $time_log
 
-profile="small" 
+profile="tiny" 
 
-#hadoop="2.10.0"
-hadoop="3.2.1"
+##benchmarks
+hadoop_bm="2.10.0"
+#hadoop_bm="3.2.1"
+
+hadoop_services="2.10.0"
+#hadoop_services="3.2.1"
 set="set1"
 
 case $profile in 
@@ -168,10 +175,9 @@ case $profile in
 esac 
 
 
-set_remote_hadoop_home  $hadoop 
+set_remote_hadoop_home  $hadoop_bm 
 
-
-date >> "timings.txt"
+time_log=/dev/null
 vmpid=0
 cleanup_bdb_cc
 run_remote_workload_bdb $vmpid "cc" "prep" "CC/genData-cc.sh $cc" $time_log $set
@@ -206,15 +212,12 @@ run_remote_workload_bdb $vmpid "wc" "prep" "wordcount/genData-wc.sh $grep" $time
 run_remote_workload_bdb $vmpid "wc" "run" "wordcount/run-wordcount.sh $grep" $time_log $set
 cleanup_bdb "wd"
 
-date >> "timings.txt"
 expunge
 
-
-mkdir -p ~/ishaan/data/bdb-hadoop/$hadoop/$profile/$set
-mv *.perf ~/ishaan/data/bdb-hadoop/$hadoop/$profile/$set
-mv $time_log ~/ishaan/data/bdb-hadoop/$hadoop/$profile/$set
-mv *.csv ~/ishaan/data/bdb-hadoop/$hadoop/$profile/$set
-mv *.pidstat ~/ishaan/data/bdb-hadoop/$hadoop/$profile/$set
+mkdir -p ~/ananth/hadoopfinal-data/bdb/${hadoop_bm}-${hadoop_services}/$profile/
+mv $time_log ~/ananth/hadoopfinal-data/bdb/${hadoop_bm}-${hadoop_services}/$profile/
+mv *.csv ~/ananth/hadoopfinal-data/bdb/${hadoop_bm}-${hadoop_services}/$profile/
+mv *.pidstat ~/ananth/hadoopfinal-data/bdb/${hadoop_bm}-${hadoop_services}/$profile/
 rm $sar_file
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/disk2/user/BigDataBench_V5.0_BigData_MicroBenchmark/BigDataGeneratorSuite/Text_datagen/gsl-1.15/.libs/:disk2/user/BigDataBench_V5.0_BigData_MicroBenchmark/BigDataGeneratorSuite/Text_datagen/gsl-1.15/cblas/.libs/
 
